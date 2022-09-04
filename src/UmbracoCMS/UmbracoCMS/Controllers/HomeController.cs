1897d2f114e5edfe65cb6fbf5dc813185ceb9822
doanhnghiepvn <manhnguyen.doanhnghiepvn@gmail.com>
2022-09-04 17:42:50 +0800

    UmbracoCMS

---
+++ src/UmbracoCMS/UmbracoCMS/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UmbracoCMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/Error
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/UmbracoCMS/UmbracoCMS/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UmbracoCMS
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error()
+        {
+            Exception exception = Server.GetLastError();
+            Server.ClearError();
+
+            Debug.WriteLine(exception);
+
+            Response.Redirect("/Home/Error");
+        }
+    }
+}
